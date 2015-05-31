0085b32cd6ce030bf2d02b643ac30862bb9ed33c
Morgan Wilde <wilde.morgan@gmail.com>
2015-05-31 19:13:04 +0300

    Update

--- sql/data/data-table-sense.sql
+++ sql/data/data-table-sense.sql
@@ -1,5 +1,5 @@
 INSERT INTO tagr1332.Sense (pronunciation, definition, part_of_speech, headword) VALUES 
-("hous", "a building for human habitation, especially one that is lived in by a family or small group of people.", "noun", "house"),
-("houz", "provide (a person or animal) with shelter or living quarters.", "verb", "house"),
-("CHānj", "make or become different.", "verb", "change"),
-("CHānj", "coins as opposed to paper currency.", "noun", "change");
+('hous', 'a building for human habitation, especially one that is lived in by a family or small group of people.', 'noun', 'house'),
+('houz', 'provide (a person or animal) with shelter or living quarters.', 'verb', 'house'),
+('CHānj', 'make or become different.', 'verb', 'change'),
+('CHānj', 'coins as opposed to paper currency.', 'noun', 'change');
