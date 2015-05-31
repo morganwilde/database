901e8739bc9c0af195b6fda27e2ddfcfa3b8c4da
Morgan Wilde <wilde.morgan@gmail.com>
2015-05-31 20:06:15 +0300

    Update

---
+++ sql/create-view-card-with-senses.sql
@@ -0,0 +1,16 @@
+CREATE VIEW tagr1332.CardWithSenses
+(card_number, option_1, option_2, option_3, sense, bucket, question, answered_correctly)
+    AS SELECT
+        card.card_number,
+        option1.headword,
+        option2.headword,
+        option3.headword,
+        sense.headword,
+        card.bucket,
+        card.question,
+        card.answered_correctly
+    FROM Card
+    JOIN Sense option1 ON option1.sense_number = card.option_1
+    JOIN Sense option2 ON option2.sense_number = card.option_2
+    JOIN Sense option3 ON option3.sense_number = card.option_3
+    JOIN Sense ON sense.sense_number = card.sense;
