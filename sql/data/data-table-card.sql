6ac3eaad500ce00bbd8f9eb6760cb95b212bb266
Morgan Wilde <wilde.morgan@gmail.com>
2015-05-31 19:52:56 +0300

    Update

--- sql/data/data-table-card.sql
+++ sql/data/data-table-card.sql
@@ -1,4 +1,4 @@
 INSERT INTO tagr1332.Card (option_1, option_2, option_3, sense, bucket, question, answered_correctly) VALUES 
-('courthouse', 'house', 'senate', 'courthouse', 'law', 'A building in which judicial cases are resolved.', NULL), 
-('judge', 'jury', 'bailiff', 'judge', 'law', 'A public official appointed to decide cases in a court of law.', NULL),
-('processor', 'transistor', 'motherboard', 'programming', 'A machine that processes something.', NULL);
+(9, 1, 12, 9, 'law', 'A building in which judicial cases are resolved.', NULL), 
+(8, 5, 7, 8, 'law', 'A public official appointed to decide cases in a court of law.', NULL),
+(14, 13, 15, 14, 'programming', 'A machine that processes something.', NULL);
