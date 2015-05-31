f0b51c463293cd3b3ccdae4823727c6b3a8fd5a3
Morgan Wilde <wilde.morgan@gmail.com>
2015-05-31 20:11:50 +0300

    Update

--- sql/create-view-card-unanswered.sql
+++ sql/create-view-card-unanswered.sql
@@ -2,10 +2,10 @@ CREATE VIEW tagr1332.CardUnanswered
 (card_number, option_1, option_2, option_3, sense, bucket, question, answered_correctly)
     AS SELECT
         card_number,
-        headword,
-        headword,
-        headword,
-        headword,
+        option_1,
+        option_2,
+        option_3,
+        sense,
         bucket,
         question
     FROM CardWithSenses
